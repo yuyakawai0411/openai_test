05802396482108bdebe0a0b07b2b5c3b6f44a81d
yuyakawai0411 <84003054+yuyakawai0411@users.noreply.github.com>
2024-04-06 17:08:57 +0900

    openaiにリクエストする雛形を作成

--- script.py
+++ script.py
@@ -1,1 +1,18 @@
-print('Hello World')
+from dotenv import dotenv_values
+from openai import OpenAI
+
+client = OpenAI(
+    api_key=dotenv_values(".env").get("OPENAI_API_KEY")
+)
+
+chat_completion = client.chat.completions.create(
+    messages=[
+        {
+            "role": "user",
+            "content": "Say this is a test",
+        }
+    ],
+    model="gpt-3.5-turbo",
+)
+
+print(chat_completion)
